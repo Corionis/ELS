294b4844b1c1069aa879bbab20adb25885e554c0
Todd Hill <rockney8588@gmail.com>
2020-09-22 12:13:46 -0600

    Updated version

--- src/com/groksoft/els/Configuration.java
+++ src/com/groksoft/els/Configuration.java
@@ -18,7 +18,7 @@ public class Configuration
     public static final int REMOTE_PUBLISH = 1;
     public static final int SUBSCRIBER_LISTENER = 2;
     public static final int SUBSCRIBER_TERMINAL = 5;
-    private final String PROGRAM_VERSION = "2.0.1";
+    private final String PROGRAM_VERSION = "2.0.2";
     private String authorizedPassword = "";
     private String consoleLevel = "debug";  // Levels: ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, and OFF
     private String debugLevel = "info";
